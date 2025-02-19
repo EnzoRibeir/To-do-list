8becf684305d186350f5be36d6fa0be342e2a2f5
Enzo Ribeiro <enzodesenveloper@gmail.com>
2025-02-19 01:55:38 -0300

    melhorando css da div de adicionar task

--- js/scripts.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('openModalButton').addEventListener('click', function() {
-    document.getElementById('overlay').style.display = 'flex';
-});
-
-document.getElementById('closeModalButton').addEventListener('click', function() {
-    document.getElementById('overlay').style.display = 'none';
-});
