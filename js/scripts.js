2170146c4296dc50192bc9d48a185b9178223fdc
Enzo Ribeiro <enzodesenveloper@gmail.com>
2025-02-19 00:34:51 -0300

    criando tela de adicionar task

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,7 @@
-console.log('Hello, World!');
+document.getElementById('openModalButton').addEventListener('click', function() {
+    document.getElementById('overlay').style.display = 'flex';
+});
+
+document.getElementById('closeModalButton').addEventListener('click', function() {
+    document.getElementById('overlay').style.display = 'none';
+});
