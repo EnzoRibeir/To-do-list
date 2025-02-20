4cbcca6f7a42ba69f1bfcc8b213a6b3216ec4d9f
Enzo Ribeiro <enzodesenveloper@gmail.com>
2025-02-20 01:26:00 -0300

    implementando funçao de adicionar task

--- js/scripts.js
+++ js/scripts.js
@@ -58,13 +58,66 @@ prevNextIcon.forEach(icon => { // obtendo ícones de anterior e próximo
 
 //back-end main
 
+
+//Seleçao dos elementos
 const btnAddTask = document.querySelector("#btn-add-task button");
 const addTaskDiv = document.querySelector("#add-task");
 const addTaskFirstDiv = document.querySelector("#add-task-first");
 const btnCalendar = document.querySelector('[name="btn-calendar"]');
 const addTaskSecondDiv = document.querySelector("#add-task-second");
+const btnExitData = document.querySelector('[name="btn-exit-data"]');
+const btnSendData = document.querySelector('[name="btn-send-data"]');
+const btnSend = document.querySelector('[name="btn-send-task"]');
+const form = document.querySelector("#add-task-form");
+const formInput = document.querySelector('[name="form-input"]');
+const formDateInput = document.querySelector("#form-date");
+
+//Funçoes
+const formatDate = (date) => {
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Os meses são baseados em zero
+    return `${day}-${month}`;
+}
+
+const saveTodo = (text, date) => {
+    const todo = document.createElement("label");
+    todo.classList.add("check-itens");
+    
+    const todoTextDiv = document.createElement("div");
+    todoTextDiv.classList.add("iten-text");
+
+    const todoCheckbox = document.createElement("input");
+    todoCheckbox.type = "checkbox";
+
+    const todoSpan = document.createElement("span");
+    todoSpan.classList.add("checkmark");
+
+    const todoTitle = document.createElement("p");
+    todoTitle.innerText = text;
+
+    todoTextDiv.appendChild(todoCheckbox);
+    todoTextDiv.appendChild(todoSpan);
+    todoTextDiv.appendChild(todoTitle);
+
+    const todoDateDiv = document.createElement("div");
+    todoDateDiv.classList.add("iten-data");
+
+    const todoDate = document.createElement("p");
+    todoDate.innerText = date;
+
+    todoDateDiv.appendChild(todoDate);
+
+    todo.appendChild(todoTextDiv);
+    todo.appendChild(todoDateDiv);
+
+    const checkList = document.querySelector("#check-list");
+    checkList.appendChild(todo);
+
+    console.log(todo);
+}
 
 
+//Eventos 
 btnAddTask.addEventListener("click", (event) => {
     addTaskSecondDiv.classList.remove("overlay");
     addTaskSecondDiv.classList.add("hide");
@@ -72,13 +125,54 @@ btnAddTask.addEventListener("click", (event) => {
 });
 
 document.addEventListener("click", (event) => {
-    if (!addTaskFirstDiv.contains(event.target) && !btnAddTask.contains(event.target)) {
+    if (!addTaskFirstDiv.contains(event.target) 
+        && !btnAddTask.contains(event.target) 
+        && !addTaskSecondDiv.contains(event.target)
+    ) {
         addTaskDiv.classList.add("hide");
     }
 });
 
 btnCalendar.addEventListener("click", (event) => {
-        addTaskSecondDiv.classList.remove("hide");
-        addTaskSecondDiv.classList.add("overlay");
-        event.preventDefault();
+    addTaskSecondDiv.classList.remove("hide");
+    addTaskSecondDiv.classList.add("overlay");
+    event.preventDefault();
+});
+
+btnExitData.addEventListener("click", (event) => {
+    formDateInput.value = ""
+    addTaskSecondDiv.classList.remove("overlay");
+    addTaskSecondDiv.classList.add("hide");
+    event.preventDefault();
+})
+
+btnSendData.addEventListener("click", (event) => {
+    addTaskSecondDiv.classList.remove("overlay");
+    addTaskSecondDiv.classList.add("hide");
+    event.preventDefault();
+})
+
+daysTag.addEventListener("click", (event) => {
+    if (event.target.tagName === "LI" && !event.target.classList.contains("inactive")) {
+        const selectedDay = event.target.innerText;
+        const selectedDate = new Date(currYear, currMonth, selectedDay);
+        formDateInput.value = formatDate(selectedDate);
+    }
+});
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const inputValue = formInput.value;
+    let dateValue = formDateInput.value;
+
+    if (!dateValue) {
+        dateValue = formatDate(new Date());
+    }
+
+    if (inputValue) {
+        saveTodo(inputValue, dateValue);
+        formInput.value = "";
+        formDateInput.value = ""; 
+    }
 });
